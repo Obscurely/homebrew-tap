bad84bdfc2167631f7523d36dd447b668b8e2fc6
Obscurely <adrian.obscurely@protonmail.com>
2022-12-13 17:25:04 +0200

    EStash: Release v0.6.2

--- Formula/estash.rb
+++ Formula/estash.rb
@@ -1,9 +1,9 @@
 class Estash < Formula
     desc "An open source, cross-platform, programmed in rust, encrypted digital vault (store files and text) with the capability to set a path and with the click of a button to copy the contents to that file."
     homepage "https://github.com/Obscurely/EStash"
-    url "https://github.com/Obscurely/EStash/releases/download/v0.6.1-stable/estash-macos.tar.gz"
-    sha256 "c6afd445c6fa364dee3d72c4608b2b9bb0a94521704d7047d3d6f8e92381798b"
-    version "0.6.1"
+    url "https://github.com/Obscurely/EStash/releases/download/v0.6.2-stable/estash-macos.tar.gz"
+    sha256 "3294f1f133ffa65a1e73426788475df0cc663a7e53b1e42bedf99e5b62d0f26a"
+    version "0.6.2"
 
     def install
         bin.install "estash"
