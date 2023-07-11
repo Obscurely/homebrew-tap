efd99d82979ad7986df25199b9b9cf8dc55790bf
Obscurely <Obscurely@users.noreply.github.com>
2023-07-11 20:35:23 +0000

    update tsfh

--- Formula/tsfh.rb
+++ Formula/tsfh.rb
@@ -1,9 +1,9 @@
 class tsfh < Formula
     desc "CHANGEME_DESC"
     homepage "https://github.com/Obscurely/tsfh"
-    url "https://github.com/Obscurely/tsfh/releases/download/v0.61.0-stable/tsfh-macos.tar.gz"
-    sha256 "425f75cbb5b7564dc824a1f60a2302ab21cbb3090cb3c1fe20c249ec9fb0e26d"
-    version "0.61.0"
+    url "https://github.com/Obscurely/tsfh/releases/download/v0.62.0-stable/tsfh-macos.tar.gz"
+    sha256 "1ce17c72337c1c043e66367dc2be077f26d04c8cde73d8485958fe18f0ebda1a"
+    version "0.62.0"
 
     def install
         bin.install "tsfh"
