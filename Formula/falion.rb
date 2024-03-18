780018566a7f832cb6146e376ecd2a48cd335f4b
Obscurely <Obscurely@users.noreply.github.com>
2024-03-18 15:40:09 +0000

    update falion

--- Formula/falion.rb
+++ Formula/falion.rb
@@ -1,9 +1,9 @@
 class falion < Formula
     desc "An open source, programmed in rust, privacy focused tool for scraping programming resources (like stackoverflow) fast, efficient and asynchronous/parallel using the CLI or GUI. "
     homepage "https://github.com/Obscurely/falion"
-    url "https://github.com/Obscurely/falion/releases/download/v1.0.0-stable/falion-macos.tar.gz"
-    sha256 "e4d1a0d0c5c17459c5bf10ebae7e69b6927a878c668decb0ffee71f56f906c35"
-    version "1.0.0"
+    url "https://github.com/Obscurely/falion/releases/download/v1.0.1-stable/falion-macos.tar.gz"
+    sha256 "295067f13d253da4363c804a772eb935a9003d2bbed19035eef3ea72d19d5c78"
+    version "1.0.1"
 
     def install
         bin.install "falion"
