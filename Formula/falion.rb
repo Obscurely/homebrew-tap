b69ac4d62de9a3ed6236a7043e7eb46bd791bc32
Obscurely <adrian.obscurely@protonmail.com>
2024-03-18 15:45:50 +0000

    Update falion.rb

--- Formula/falion.rb
+++ Formula/falion.rb
@@ -1,4 +1,4 @@
-class falion < Formula
+class Falion < Formula
     desc "An open source, programmed in rust, privacy focused tool for scraping programming resources (like stackoverflow) fast, efficient and asynchronous/parallel using the CLI or GUI. "
     homepage "https://github.com/Obscurely/falion"
     url "https://github.com/Obscurely/falion/releases/download/v1.0.1-stable/falion-macos.tar.gz"
